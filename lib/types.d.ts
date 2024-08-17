7bc1e44948c4b35025be56fbbe6455e872b00f6e
Shun Git <kwokshungit@gmail.com>
2024-08-17 14:47:50 +0800

    update useStorage

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,3 +1,5 @@
+export type JSONHelper = (key: string, val: unknown) => unknown
+
 export type PaginationOpts = {
 	/**
 	 * default page size (default: 10)
@@ -17,4 +19,18 @@ export type SelectionOpts<ItemType, IdType> = {
 	 * function or property key to get the id for an item
 	 */
 	getId?: keyof ItemType | ((i: ItemType) => IdType)
+}
+
+export type StorageOpts<StoreType> = {
+	/** storage key (default: `use-storage-store`) */
+	key?: string
+
+	/** initial values */
+	initial?: StoreType
+
+	/** JSON.parse reviver */
+	reviver?: JSONHelper
+
+	/** JSON.stringify replacer */
+	replacer?: JSONHelper
 }
