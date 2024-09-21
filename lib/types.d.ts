9aa647fdeb615e42e6bdc2776f0307648b547109
Shun Git <kwokshungit@gmail.com>
2024-09-21 17:41:35 +0800

    useSorting

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,4 +1,59 @@
+import { VNode } from 'vue'
+
 export type JSONHelper = (key: string, val: unknown) => unknown
+
+export type Renderable = string | number | VNode | VNode[]
+
+/** @see https://github.com/vuejs/core/pull/8012 */
+export type ClassValue =
+	| string
+	| Record<string | number, unknown>
+	| ClassValue[]
+
+export type Comparable = number | boolean | bigint | string | Date
+
+export type Comparator<ItemType> = (
+	a: ItemType,
+	b: ItemType,
+	dir: number
+) => number
+
+export interface ColumnMeta {
+	/** class for headers */
+	thClass?: ClassValue
+
+	/** class for cells */
+	tdClass?: ClassValue
+}
+
+export type ColumnDefinition<ItemType, ColKey extends string> = {
+	/** column key */
+	key: ColKey
+
+	/** metadata */
+	meta?: ColumnMeta
+
+	/** comparison function */
+	cmp?: Comparator<ItemType>
+
+	/**
+	 * value to use for sorting
+	 *
+	 * ignored if `cmp` is provided
+	 */
+	sortBy?: string | keyof ItemType | ((i: ItemType) => Comparable)
+
+	/**
+	 * how to treat nullish values (default: `last`)
+	 *
+	 * ignored if `cmp` is provided
+	 *
+	 * `small`/`large` indicates that they will be smaller/larger than any other values
+	 *
+	 * `first`/`last` will always put them first/last regardless of sort direction
+	 */
+	nulls?: 'small' | 'large' | 'first' | 'last'
+}
 
 export type PaginationOpts = {
 	/**
